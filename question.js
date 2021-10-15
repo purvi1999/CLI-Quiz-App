b6f08e2175a4afa9e6133df392f833ff83b24daf
Purvi Patel <91878359+purvi1999@users.noreply.github.com>
2021-10-15 11:43:55 +0530

    Create question.js

---
+++ question.js
@@ -0,0 +1,94 @@
+const queArray=[
+  {
+    id:"1",
+    que:"An electronic tool that allows information to be input, processed, and output is called _______________. ",
+    A:"Operating system",
+    B:"Motherboard",
+    C:"Computer",
+    D:"CPU",
+    ans:"C"
+  },
+  {
+    id:"2",
+    que:"____________ is a worldwide network of computers.",
+    A:"CPU",
+    B:"Internet",
+    C:"RAM",
+    D:"network",
+    ans:"B"
+  },
+  {
+    id:"3",
+    que:"Part of a computer that allows a user to put information into the computer is called ______________. ",
+    A:"Output Device",
+    B:"Software",
+    C:"Operating System",
+    D:"Input Device",
+    ans:"D"
+  },
+  {
+    id:"4",
+    que:"Name the brain of the computer that does the calculation, moving, and processing of information. ",
+    A:"CPU",
+    B:"RAM",
+    C:"Motherboard",
+    D:"Hard Drive",
+    ans:"A"
+  },
+  {
+    id:"5",
+    que:"A small picture that represents a folder, program or other things is known as ______________.",
+    A:"Desktop",
+    B:"Icon",
+    C:"Graphic",
+    D:"Image",
+    ans:"B"
+  },
+  {
+    id:"6",
+    que:"What is the name given to the computer's short-term memory that is lost when the computer is turned off?",
+    A:"CPU",
+    B:"Hardware",
+    C:"RAM",
+    D:"Processor",
+    ans:"C"
+  },
+  {
+    id:"7",
+    que:"Name the computer part that is connected to all other aspects of a computer and allows them to communicate and work together. ",
+    A:"Operating System",
+    B:"Disk Drive",
+    C:"Output Device",
+    D:"Motherboard",
+    ans:"D"
+  },
+  {
+    id:"8",
+    que:"The physical parts of a computer are termed as ______________.",
+    A:"Hardware",
+    B:"Hard Drive",
+    C:"Disk Drive",
+    D:"Software",
+    ans:"A"
+  },
+  {
+    id:"9",
+    que:"Parts of a computer that allow the user to see or hear information that comes out from the computer are called ____________________. ",
+    A:"Software",
+    B:"Input Device",
+    C:"Output Device",
+    D:"Operating System",
+    ans:"C"
+  },
+  {
+    id:"10",
+    que:"Another name for computer programs is ____________________. ",
+    A:"Software",
+    B:"RAM",
+    C:"Input Devices",
+    D:"Hardware",
+    ans:"A"
+  }
+];
+module.exports=queArray;
+//console.log(module.exports);
